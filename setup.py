9123ba1238996c65f324032379e97765fcb9f5b4
Nick-Kou <51099935+Nick-Kou@users.noreply.github.com>
2021-11-21 12:21:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Nick-Kou/minisampy.git'
 EMAIL = 'nkourtza@ryerson.ca'
 AUTHOR = 'Nikolaos Kourtzanidis'
 REQUIRES_PYTHON = '==3.7'
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
